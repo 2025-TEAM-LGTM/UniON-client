7d83003ef6eb1b51ad89344d7e59664fe9f9281e
eunkr82 <eun.kr82@gmail.com>
2026-03-03 18:53:30 +0900

    feat: postgroup 페이지에 추가

---
+++ src/pages/posts/post.css.ts
@@ -0,0 +1,43 @@
+import { themeVars } from '@shared/styles';
+import { style } from '@vanilla-extract/css';
+
+export const bannerContainer = style({
+  marginTop: '2rem',
+  padding: '0 6.9rem',
+});
+
+export const pageContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100dvh',
+  padding: '0 15rem',
+});
+
+export const sectionHeader = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  margin: '2rem 0',
+  color: themeVars.color.gray_800,
+  ...themeVars.font.body_20sb,
+});
+
+export const writeButtonContainer = style({
+  width: '15.2rem',
+});
+
+export const dropdownContainer = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginBottom: '2rem',
+});
+
+export const dropdown = style({
+  display: 'flex',
+  gap: '1rem',
+});
+
+export const searchButtonContainer = style({
+  display: 'flex',
+  gap: '1rem',
+});
