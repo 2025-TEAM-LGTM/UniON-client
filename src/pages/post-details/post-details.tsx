f03935ff0b54566d4798f354c8623944340f9952
eunkr82 <eun.kr82@gmail.com>
2026-03-05 02:56:27 +0900

    feat: post-details-header 컴포넌트 구현

--- src/pages/post-details/post-details.tsx
+++ src/pages/post-details/post-details.tsx
@@ -1,5 +1,22 @@
+import { MOCK_POST_DETAIL } from './mock/mock-post-detail';
+import * as styles from './post-details.css';
+import { toPostDetailHeaderProps } from './widgets/post-detail-header/adapters';
+import PostDetailHeader from './widgets/post-detail-header/post-details-header';
+
 const PostDetailPage = () => {
-  return <div>PostDetailPage</div>;
+  const res = MOCK_POST_DETAIL;
+  const headerProps = toPostDetailHeaderProps(res);
+
+  return (
+    <section className={styles.headerContainer}>
+      <PostDetailHeader
+        {...headerProps}
+        onBackClick={() => {
+          window.history.back();
+        }}
+      />
+    </section>
+  );
 };
 
 export default PostDetailPage;
