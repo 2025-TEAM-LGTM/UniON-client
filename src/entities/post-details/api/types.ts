8d2db535601d4e2c1be544bc19ec82d53d1a1948
eunkr82 <eun.kr82@gmail.com>
2026-03-07 04:40:28 +0900

    fix: API 수정사항 반영해 types.ts 수정

--- src/entities/post-details/api/types.ts
+++ src/entities/post-details/api/types.ts
@@ -10,15 +10,23 @@ export interface PostDetailsResponse {
       userImageUrl: string | null;
     };
     title: string;
-    domainIds: number[];
+    domains: Array<{
+      // TODO: API 확정 후 수정
+      id: number;
+      name: string;
+    }>;
     recruitPeriod: {
       startDate: string;
       endDate: string;
     };
-    homepageUrl: string;
+    homepageUrl?: string;
     contact: string;
-    currentRoles: Array<{ roleId: number; count: number }>;
-    recruitRoles: Array<{ roleId: number; count: number }>;
+    recruits: Array<{
+      // TODO: API 확정 후 수정
+      roleId: number;
+      roleName: string;
+      roleCount: number;
+    }>;
     seeking: string;
     aboutUs: string;
     teamCulture: Record<string, number>;
