4dba374fe02dff94c865d8080e418cd2b0754a02
eunkr82 <eun.kr82@gmail.com>
2026-03-19 04:23:55 +0900

    배너 컴포넌트 포함 전체 레이아웃 구현

--- src/pages/members/members.tsx
+++ src/pages/members/members.tsx
@@ -1,5 +1,21 @@
+import Banner from '@shared/ui/components/banner/banner';
+
+import * as styles from './members.css';
+
 const MembersPage = () => {
-  return <div>MembersPage</div>;
+  return (
+    <>
+      <section className={styles.bannerContainer}>
+        <Banner />
+      </section>
+
+      <main className={styles.pageContainer}>
+        <section className={styles.sectionHeader}>
+          <p>전체 팀원 모아보기</p>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default MembersPage;
