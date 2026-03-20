9591ca262d053cb5a7926354b46674308e8af600
eunkr82 <eun.kr82@gmail.com>
2026-03-20 14:18:05 +0900

    feat: member-card-group 컴포넌트 구현

--- src/pages/members/members.tsx
+++ src/pages/members/members.tsx
@@ -1,8 +1,13 @@
+import { toMemberCardModels } from '@entities/members/model/adapters';
 import Banner from '@shared/ui/components/banner/banner';
 
 import * as styles from './members.css';
+import { MOCK_MEMBERS_RESPONSE } from './mocks/mock-members';
+import MemberCardGroup from './widgets/member-card-group/member-card-group';
 
 const MembersPage = () => {
+  const members = MOCK_MEMBERS_RESPONSE.data.members;
+  const memberCardModels = toMemberCardModels(members);
   return (
     <>
       <section className={styles.bannerContainer}>
@@ -13,6 +18,7 @@ const MembersPage = () => {
         <section className={styles.sectionHeader}>
           <p>전체 팀원 모아보기</p>
         </section>
+        <MemberCardGroup members={memberCardModels} />
       </main>
     </>
   );
