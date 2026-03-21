ac0d1934e595f33bd9d1e809fc651ee582cf36fc
eunkr82 <eun.kr82@gmail.com>
2026-03-22 00:04:45 +0900

    feat: 성향 정보 key 추가

---
+++ src/shared/utils/team-culture/team-culture-keys.ts
@@ -0,0 +1,18 @@
+export const TEAM_CULTURE_KEYS = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+];
+
+export type TeamCultureKey = (typeof TEAM_CULTURE_KEYS)[number];
