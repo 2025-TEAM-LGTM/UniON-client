40d25447f326251041f42c63f3a9f9e02ff530b5
eunkr82 <eun.kr82@gmail.com>
2026-03-25 01:10:21 +0900

    remove: 불필요 속성 삭제

--- src/features/posts/model/filters-state.ts
+++ src/features/posts/model/filters-state.ts
@@ -1,13 +1,11 @@
 export interface FiltersState {
   domainIds: number[];
   fieldId: number | null;
-  skillIds: number[];
   roleIds: number[];
 }
 
 export const EMPTY_FILTERS: FiltersState = {
   domainIds: [],
   fieldId: null,
-  skillIds: [],
   roleIds: [],
 };
