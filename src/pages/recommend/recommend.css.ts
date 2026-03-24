96d7730973234faea0dab4c6dcf6e6df024f5aff
eunkr82 <eun.kr82@gmail.com>
2026-03-25 01:52:39 +0900

    feat: 기본 레이아웃 추가

---
+++ src/pages/recommend/recommend.css.ts
@@ -0,0 +1,39 @@
+import { themeVars } from '@shared/styles';
+import { style } from '@vanilla-extract/css';
+
+export const bannerContainer = style({
+  marginTop: '2rem',
+  padding: '0 2rem',
+  '@media': {
+    'screen and (min-width: 1024px)': {
+      padding: '0 8rem',
+    },
+    'screen and (min-width: 1440px)': {
+      padding: '0 15rem',
+    },
+  },
+});
+
+export const pageContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100dvh',
+  padding: '0 2rem 4rem',
+  '@media': {
+    'screen and (min-width: 1024px)': {
+      padding: '0 8rem 4rem',
+    },
+    'screen and (min-width: 1440px)': {
+      padding: '0 15rem 4rem',
+    },
+  },
+});
+
+export const sectionHeader = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  margin: '2rem 0',
+  color: themeVars.color.gray_800,
+  ...themeVars.font.body_20sb,
+});
--- src/pages/recommend/recommend.tsx
+++ src/pages/recommend/recommend.tsx
@@ -1,5 +1,21 @@
+import Banner from '@shared/ui/components/banner/banner';
+
+import * as styles from './recommend.css';
+
 const RecommendPage = () => {
-  return <div>RecommendPage</div>;
+  return (
+    <>
+      <section className={styles.bannerContainer}>
+        <Banner />
+      </section>
+
+      <main className={styles.pageContainer}>
+        <section className={styles.sectionHeader}>
+          <p>핏한 팀원 추천받기</p>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default RecommendPage;
