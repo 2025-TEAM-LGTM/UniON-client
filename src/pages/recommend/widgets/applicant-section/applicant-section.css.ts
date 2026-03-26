320a32c6d1b36df2b352aa1b00a6f6954ca2a35f
eunkr82 <eun.kr82@gmail.com>
2026-03-27 00:44:36 +0900

    chore: 오타 수정

--- src/pages/recommend/widgets/applicant-section/applicant-section.css.ts
+++ src/pages/recommend/widgets/applicant-section/applicant-section.css.ts
@@ -13,6 +13,7 @@ export const sectionTitle = style({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
+  marginTop: '1rem',
   color: themeVars.color.gray_800,
   ...themeVars.font.body_20sb,
 });
