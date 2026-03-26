f1372a6fb685b0d1592849cb0804d9e67b5a0594
eunkr82 <eun.kr82@gmail.com>
2026-03-26 23:04:32 +0900

    feat: applicant api 응답 타입 추가

---
+++ src/entities/applicant/api/types.ts
@@ -0,0 +1,27 @@
+import type { SuccessResponse } from '@shared/api/types';
+import type { Personality } from '@shared/utils/personality/types';
+
+export interface ApplicantMemberRole {
+  id: number;
+  name: string;
+}
+
+export interface ApplicantMemberSkill {
+  id: number;
+  name: string;
+}
+
+export interface ApplicantMemberResponse {
+  userId: number;
+  username: string;
+  imageUrl: string | null;
+  role: ApplicantMemberRole;
+  hardSkill: ApplicantMemberSkill[];
+  personality: Personality;
+}
+
+export interface GetApplicantsResponseData {
+  members: ApplicantMemberResponse[];
+}
+
+export type GetApplicantsResponse = SuccessResponse<GetApplicantsResponseData>;
