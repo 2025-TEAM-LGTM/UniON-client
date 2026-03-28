1e139d611703452f6999b6e0a5a14aadbcb22176
eunkr82 <eun.kr82@gmail.com>
2026-03-29 05:11:01 +0900

    feat: 프로필 기본 정보 컴포넌트 구현

---
+++ src/widgets/profile/profile-summary-card/profile-summary-card.css.ts
@@ -0,0 +1,31 @@
+import { themeVars } from '@shared/styles';
+import { style } from '@vanilla-extract/css';
+
+export const sectionContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '2.4rem 3rem',
+  gap: '0.9em',
+  border: `1px solid ${themeVars.color.gray_200}`,
+  borderRadius: '10px',
+});
+
+export const profileImageContainer = style({
+  width: '7.2rem',
+  height: '7.2rem',
+});
+
+export const profileImage = style({
+  borderRadius: '50%',
+  objectFit: 'cover',
+});
+
+export const username = style({
+  color: themeVars.color.gray_900,
+  ...themeVars.font.title_28sb,
+});
+
+export const userInfo = style({
+  color: themeVars.color.gray_400,
+  ...themeVars.font.body_18sb,
+});
---
+++ src/widgets/profile/profile-summary-card/profile-summary-card.tsx
@@ -0,0 +1,45 @@
+import { IMAGES } from '@shared/assets/images';
+import { useState } from 'react';
+
+import * as styles from './profile-summary-card.css';
+
+interface ProfileSummaryCardProps {
+  username: string;
+  birthYearLabel: string;
+  mainRoleName: string;
+  profileImageUrl: string | null;
+}
+
+const ProfileSummaryCard = ({
+  username,
+  birthYearLabel,
+  mainRoleName,
+  profileImageUrl,
+}: ProfileSummaryCardProps) => {
+  const [hasImageError, setHasImageError] = useState(false);
+
+  const handleImageError = () => {
+    setHasImageError(true);
+  };
+
+  const isValidImage = profileImageUrl != null && !hasImageError;
+  const imageSrc = isValidImage ? profileImageUrl : IMAGES.PROFILE;
+
+  return (
+    <section className={styles.sectionContainer}>
+      <div className={styles.profileImageContainer}>
+        <img
+          src={imageSrc}
+          alt={`${username} 프로필 이미지`}
+          className={styles.profileImage}
+          onError={handleImageError}
+        />
+      </div>
+      <p className={styles.username}>{username}</p>
+      <p className={styles.userInfo}>{birthYearLabel}</p>
+      <p className={styles.userInfo}>{mainRoleName}</p>
+    </section>
+  );
+};
+
+export default ProfileSummaryCard;
