d1a185b834cd1bfe9a6ee9f5e0fd0995afd019c2
eunkr82 <eun.kr82@gmail.com>
2026-03-29 23:45:17 +0900

    feat: 성향정보 컴포넌트 추가

---
+++ src/widgets/profile/profile-personality-card/profile-personality-card.tsx
@@ -0,0 +1,28 @@
+import Chip from '@shared/ui/components/chip/chip';
+import type { PersonalityItem } from '@shared/utils/personality/to-personality-items';
+
+import ProfileSection from '../profile-section/profile-section';
+import * as styles from './profile-personality-card.css';
+
+interface ProfilePersonalityCardProps {
+  personalityItems: PersonalityItem[];
+}
+
+const ProfilePersonalityCard = ({
+  personalityItems,
+}: ProfilePersonalityCardProps) => {
+  return (
+    <ProfileSection title='성향 정보'>
+      <div className={styles.personalityItemContainer}>
+        {personalityItems.map((item) => (
+          <div key={item.key} className={styles.personalityItem}>
+            <p className={styles.skillTitle}>{item.title}</p>
+            <Chip>{item.selectedLabel}</Chip>
+          </div>
+        ))}
+      </div>
+    </ProfileSection>
+  );
+};
+
+export default ProfilePersonalityCard;
