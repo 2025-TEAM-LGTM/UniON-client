1e4c3875a7d7859c3958e87aca1657b927215a79
eunkr82 <eun.kr82@gmail.com>
2026-03-31 17:21:14 +0900

    feat: API 원본 응답 타입 정의

---
+++ src/entities/post-create/api/types.ts
@@ -0,0 +1,30 @@
+import type { TeamCulture } from '@shared/utils/team-culture/types';
+
+export interface RecruitPeriodRequest {
+  startDate: string;
+  endDate: string;
+}
+
+export interface RoleCountRequest {
+  roleId: number;
+  count: number;
+}
+
+export interface CreatePostRequest {
+  title: string;
+  domainIds: number[];
+  recruitPeriod: RecruitPeriodRequest;
+  homepageUrl: string;
+  contact: string;
+  currentRoles: RoleCountRequest[];
+  recruitRoles: RoleCountRequest[];
+  aboutUs: string;
+  teamCulture: TeamCulture;
+  seeking: string;
+  imageKey: string;
+  imageSize: number;
+}
+
+export interface CreatePostResponseData {
+  postId: number;
+}
