5cc5090072d0eda0783c02ed5fed6ebc6d3f037b
eunkr82 <eun.kr82@gmail.com>
2026-03-31 12:40:47 +0900

    feat: 공통 헤더 컴포넌트 페이지 레이아웃에 추가

---
+++ src/pages/post-create/post-create.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const headerContainer = style({
+  display: 'flex',
+  marginTop: '4.8rem',
+  padding: '0 14.8rem',
+});
--- src/pages/post-create/post-create.tsx
+++ src/pages/post-create/post-create.tsx
@@ -1,5 +1,18 @@
+import { ROUTE_PATH } from '@shared/constants/path';
+import PageBackHeader from '@widgets/page-back-header/page-back-header';
+
+import * as styles from './post-create.css';
+
 const PostCreatePage = () => {
-  return <div>PostCreatePage</div>;
+  return (
+    <section className={styles.headerContainer}>
+      <PageBackHeader
+        title='글쓰기'
+        backLabel='목록으로'
+        fallbackPath={ROUTE_PATH.POSTS}
+      />
+    </section>
+  );
 };
 
 export default PostCreatePage;
