01fe2648064c0896d77c996cc8f9075719780ca5
eunkr82 <eun.kr82@gmail.com>
2026-04-03 03:30:49 +0900

    fix: 누락된 임포트 추가

--- src/pages/my-portfolio-details/my-portfolio-details.tsx
+++ src/pages/my-portfolio-details/my-portfolio-details.tsx
@@ -1,4 +1,5 @@
 import { toPortfolioDetailViewModel } from '@entities/my-portfolio-details/model/adapters';
+import { ROUTE_PATH } from '@shared/constants/path';
 // import { ROUTE_BUILDER } from '@shared/constants/path';
 import CtaButton from '@shared/ui/components/cta-button/cta-button';
 import { useToast } from '@shared/ui/components/toast/toast-context';
