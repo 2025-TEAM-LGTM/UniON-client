cdb5a7ac1667aeb1d4dff6129c5e5ad3eeddf07f
eunkr82 <eun.kr82@gmail.com>
2026-04-04 03:02:23 +0900

    fix: 공고 모아보기 목데이터 통일

--- src/pages/posts/mock/mock-posts-response.ts
+++ src/pages/posts/mock/mock-posts-response.ts
@@ -10,12 +10,20 @@ export const MOCK_POST_RESPONSE: GetPostsResponse = {
         title: '대학생 연합 해커톤 서비스 프론트엔드 구합니다',
         dday: 5,
         domains: [
-          { id: 1, name: 'IT/개발' },
+          { id: 1, name: 'IT' },
           { id: 3, name: '창업' },
         ],
         recruits: [
-          { roleId: 101, roleName: '프론트엔드', roleCount: 2 },
-          { roleId: 301, roleName: '기획', roleCount: 1 },
+          {
+            roleId: 301,
+            roleName: 'Frontend developer',
+            roleCount: 2,
+          },
+          {
+            roleId: 302,
+            roleName: 'Backend developer',
+            roleCount: 1,
+          },
         ],
         nowCount: 3,
         applied: false,
@@ -26,8 +34,16 @@ export const MOCK_POST_RESPONSE: GetPostsResponse = {
         dday: 12,
         domains: [{ id: 2, name: '디자인' }],
         recruits: [
-          { roleId: 201, roleName: '브랜딩 디자이너', roleCount: 1 },
-          { roleId: 202, roleName: 'UIUX 디자이너', roleCount: 1 },
+          {
+            roleId: 401,
+            roleName: 'UI/UX Designer',
+            roleCount: 1,
+          },
+          {
+            roleId: 402,
+            roleName: 'Graphic Designer',
+            roleCount: 1,
+          },
         ],
         nowCount: 2,
         applied: true,
@@ -41,8 +57,16 @@ export const MOCK_POST_RESPONSE: GetPostsResponse = {
           { id: 4, name: '마케팅' },
         ],
         recruits: [
-          { roleId: 301, roleName: '기획', roleCount: 1 },
-          { roleId: 401, roleName: '마케터', roleCount: 2 },
+          {
+            roleId: 105,
+            roleName: 'PR/Marketing Planner',
+            roleCount: 1,
+          },
+          {
+            roleId: 103,
+            roleName: 'Digital Marketer',
+            roleCount: 2,
+          },
         ],
         nowCount: 4,
         applied: false,
@@ -51,10 +75,18 @@ export const MOCK_POST_RESPONSE: GetPostsResponse = {
         postId: 4,
         title: '앱 런칭용 백엔드/프론트엔드 모집',
         dday: 9,
-        domains: [{ id: 1, name: 'IT/개발' }],
+        domains: [{ id: 1, name: 'IT' }],
         recruits: [
-          { roleId: 101, roleName: '프론트엔드', roleCount: 1 },
-          { roleId: 102, roleName: '백엔드', roleCount: 1 },
+          {
+            roleId: 301,
+            roleName: 'Frontend developer',
+            roleCount: 1,
+          },
+          {
+            roleId: 302,
+            roleName: 'Backend developer',
+            roleCount: 1,
+          },
         ],
         nowCount: 2,
         applied: false,
