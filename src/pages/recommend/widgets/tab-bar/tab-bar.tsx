519a628db785db677838dc9c1e1299e9725c63da
eunkr82 <eun.kr82@gmail.com>
2026-04-06 18:24:02 +0900

    feat: 탭 컴포넌트 구현

---
+++ src/pages/recommend/widgets/tab-bar/tab-bar.tsx
@@ -0,0 +1,32 @@
+import * as styles from './tab-bar.css';
+
+export type TabLabelTypes = 'recommend' | 'applicant';
+
+interface TabBarProps {
+  activeTab: TabLabelTypes;
+  onChangeTab: (tab: TabLabelTypes) => void;
+}
+
+const TabBar = ({ activeTab, onChangeTab }: TabBarProps) => {
+  return (
+    <div className={styles.tabContainer}>
+      <button
+        type='button'
+        className={styles.tabButton({ isActive: activeTab === 'recommend' })}
+        onClick={() => onChangeTab('recommend')}
+      >
+        팀원 추천받기
+      </button>
+      <div className={styles.divider} />
+      <button
+        type='button'
+        className={styles.tabButton({ isActive: activeTab === 'applicant' })}
+        onClick={() => onChangeTab('applicant')}
+      >
+        지원한 팀원 보기
+      </button>
+    </div>
+  );
+};
+
+export default TabBar;
