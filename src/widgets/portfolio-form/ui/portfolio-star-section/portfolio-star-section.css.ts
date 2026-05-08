91ed3b8d2c74ba8fb170450e1c93a036caebd8d2
eunkr82 <eun.kr82@gmail.com>
2026-05-09 02:50:18 +0900

    feat: STAR 포트폴리오 섹션 컴포넌트 작성

---
+++ src/widgets/portfolio-form/ui/portfolio-star-section/portfolio-star-section.css.ts
@@ -0,0 +1,31 @@
+import { themeVars } from '@shared/styles';
+import { style } from '@vanilla-extract/css';
+
+export const sectionContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  paddingBottom: '3.1rem',
+  gap: '2rem',
+  width: '100%',
+});
+
+export const sectionTitle = style({
+  ...themeVars.font.body_20sb,
+  color: themeVars.color.gray_800,
+});
+
+export const required = style({
+  color: themeVars.color.primary,
+});
+
+export const list = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2.2rem',
+});
+
+export const card = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+});
