0bf6b5232f5ce2e6c342d9a9d1253d92a191de6f
eunkr82 <eun.kr82@gmail.com>
2026-05-09 15:15:31 +0900

    feat: 목데이터 추가

---
+++ src/pages/signup/mock/mock-signup-universities.ts
@@ -0,0 +1,12 @@
+import type { UniversityOption } from '@shared/ui/components/dropdown/dropdown-content/university-content';
+
+export const MOCK_UNIVERSITIES: UniversityOption[] = [
+  { id: 101, name: '가야대학교(고령)' },
+  { id: 102, name: '가야대학교(김해)' },
+  { id: 103, name: '가천대학교' },
+  { id: 104, name: '가톨릭관동대학교' },
+  { id: 105, name: '연세대학교' },
+  { id: 106, name: '이화여자대학교' },
+  { id: 107, name: '서울대학교' },
+  { id: 108, name: '감리교신학대학교' },
+];
