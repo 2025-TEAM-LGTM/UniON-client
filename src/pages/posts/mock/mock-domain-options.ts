ea9d1c269afd30001f72e3d0d39fb42254523d3c
eunkr82 <eun.kr82@gmail.com>
2026-05-11 18:04:52 +0900

    remove: 목데이터 삭제

--- src/pages/posts/mock/mock-domain-options.ts
+++
@@ -1,8 +0,0 @@
-import type { Option } from '@shared/types/common';
-
-export const MOCK_DOMAIN_OPTIONS: Option[] = [
-  { id: 1, name: 'IT' },
-  { id: 2, name: '디자인' },
-  { id: 3, name: '창업' },
-  { id: 4, name: '마케팅' },
-];
