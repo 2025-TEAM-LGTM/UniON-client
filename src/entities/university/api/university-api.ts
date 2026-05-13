67662344a83148d3f3b09a2a6a8727a7006725bb
eunkr82 <eun.kr82@gmail.com>
2026-05-13 15:08:29 +0900

    fix: 엔드포인트 수정

--- src/entities/university/api/university-api.ts
+++ src/entities/university/api/university-api.ts
@@ -2,5 +2,8 @@ import { get } from '@shared/api/http';
 import type { Option } from '@shared/types/common';
 
 export const getUniversities = (query?: string): Promise<Option[]> => {
-  return get<Option[]>('/api/university', query ? { q: query } : undefined);
+  return get<Option[]>(
+    '/api/dropdown/universities',
+    query ? { q: query } : undefined,
+  );
 };
